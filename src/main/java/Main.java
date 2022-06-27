4b3fb775f128d413696f47b458416096f4aada5f
jjheffernan <jjheffernan99@gmail.com>
2022-06-27 03:58:33 -0400

    added in variables

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 /**
- * Created by iyasuwatts on 10/17/17.
+ * Re-created by jjheffernan on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+        String user1 = "Alice";
+        String user2 = "Bob";
+
+        
 
     }
 }
